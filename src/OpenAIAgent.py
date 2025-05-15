bde111e73362b11f8b70013ed2b6cc3bdd36a460
Kishen Sharma <ksharma06@pm.me>
2025-05-15 16:41:02 -0700

    First

---
+++ src/OpenAIAgent.py
@@ -0,0 +1,19 @@
+import openai 
+
+class Agent:
+    def __init__(self, api_key, model = "gpt-4.1", name= "OpenAIAgent"):
+        """Initialize an agent with OpenAI API key and model
+
+        Args:
+            api_key (string): API key from OpenAI API platform
+            model (str, optional): OpenAI model you would like agent to use. 
+            Defaults to "gpt-4.1".
+            name (str, optional): Name of agent. Defaults to "OpenAIAgent".
+        """
+        self.api_key = api_key
+        self.model = model
+        self.name = name
+
+    def get_response(self, prompt,role, temp = .7, max_tokens = 150):
+
+        response = openai.ChatCompletion.create()
