8215fb5cc845e9215ba3e2d128062c49be0eda0c
Kishen Sharma <ksharma06@pm.me>
2025-05-20 16:25:08 -0700

    created GmailWriter base class outline

---
+++ src/gmail/GmailWriter.py
@@ -0,0 +1,20 @@
+import base64
+from email.message import EmailMessage
+
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+class GmailWriter:
+    def __init__(self):
+        pass
+
+    def write_email(self):
+        pass
+    
+    def write_email_with_attachment(self):
+        pass
+
+    def send_message(self):
+        pass
