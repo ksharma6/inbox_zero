d6128a2c06b310cc61d7d3ded1ae97559a095b28
Kishen Sharma <ksharma06@pm.me>
2025-05-19 17:38:48 -0700

    Added tools and agent logic

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+
+from src.agent.OpenAIAgent import Agent
+from src.gmail.GmailReader import GmailReader
+from src.utils.load_dotenv import load_dotenv_helper
+
+#load keys + paths
+load_dotenv_helper()
+token_path = os.getenv("TOKENS_PATH")
+api_path=os.getenv("OPENAI_API_KEY")
+
+#Init objects
+reader = GmailReader(token_path)
+
+tools = [{
+    "type": "function",
+
+    "name": "gmail_reader_read_labels",
+    "description": "Get current labels from gmail email account.",
+    "parameters": {
+        # "type": "object",
+        # "properties": {
+        #     "location": {
+        #         "type": "string",
+        #         "description": "City and country e.g. Bogotá, Colombia"
+            }
+        ,
+    "additionalProperties": False}]
+    
+available_tools = {
+    "gmail_reader_read_labels": reader.read_labels
+}
+
+agent = Agent(api_key=token_path,
+              instructions="You are a helpful AI agent",
+              tools=tools)
+
