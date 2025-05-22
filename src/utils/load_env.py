96b67b88338e1a9aa546e65777b74003a6ad756e
Kishen Sharma <ksharma06@pm.me>
2025-05-21 17:30:23 -0700

    Renamed

---
+++ src/utils/load_env.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+def load_dotenv_helper(path = None):
+    if load_dotenv(path):
+        print("Successfully loaded .env (default search).")
+    else:
+        project_root_env_alt = Path.cwd() / ".env" 
+        if project_root_env_alt.exists():
+            load_dotenv(dotenv_path=project_root_env_alt)
+            print(f"Loaded .env from CWD: {project_root_env_alt}")
+        else:
+            print("Warning: .env file not found via default search or in CWD.")
