b1fb5c9c0c4762479fedd66d6c9587e63f9d4990
Kishen Sharma <ksharma06@pm.me>
2025-08-24 12:04:13 -0700

    updated docstrings and import

--- src/LangGraph/workflow_factory.py
+++ src/LangGraph/workflow_factory.py
@@ -1,23 +1,27 @@
-from src.gmail.GmailReader import GmailReader
-from src.gmail.GmailWriter import GmailWriter
-from src.slack.DraftApprovalHandler import get_draft_handler
-from src.LangGraph.workflow import EmailProcessingWorkflow
-from openai import OpenAI
 import os
+
+from openai import OpenAI
 from slack_bolt import App
+
+from src.gmail import GmailReader, GmailWriter
+from src.LangGraph.workflow import EmailProcessingWorkflow
+from src.slack.DraftApprovalHandler import get_draft_handler
 
 
 def get_workflow(slack_app: App):
-    """Initialize EmailProcessingWorkflow with all dependencies and return to user
+    """Initialize EmailProcessingWorkflow with all dependencies outlined in .env file and return to user
 
     Returns:
-        EmailProcessingWorkflow: A configured workflow instance with all dependencies
+        EmailProcessingWorkflow: A configured workflow instance
+
+    Example:
+        workflow = get_workflow(slack_app)
+        workflow.run()
     """
     gmail_token = os.getenv("TOKENS_PATH")
     gmail_writer = GmailWriter(gmail_token)
     gmail_reader = GmailReader(gmail_token)
 
-    # # Use the same draft handler instance
     draft_handler = get_draft_handler(slack_app)
 
     openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
